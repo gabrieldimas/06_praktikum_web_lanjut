3b78274803b3e4fff450d96bdca76f143d4b7b8e
Gabriel Dimas <93063866+gabrieldimas@users.noreply.github.com>
2023-05-11 12:58:22 +0700

    jobsheet 6

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show'])
+    ->name('posts.show');
+
+Route::get('/fat', [PostController::class, 'index']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
